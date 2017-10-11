a453b478b37bcceb898e0c682817e647e8f284e0
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-10-11 15:00:10 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 using namespace std;
+int sumofarray(int a[10])
+{
+  int sum=0;
+	for(int i=0;i<10;i++)
+	{
+	   sum+=a[i];
+	}
+  cout<<sum;
+}
 int main()
 {
-	int a[10];
-	cout<<"Enter 10 numbers";
+  int a[10];
+  cout<<"Enter 10 numbers";
 	for(int i=0;i<10;i++)
 	{
 	   cout<<i<<"-";
 	   cin>>a[i];
 	   cout<<endl;
+	   
 	}
-	int sum=0;
-	for(int i=0;i<10;i++)
-	{
-	   sum+=a[i];
-	}
-	cout<<sum;
+	sumofarray(a);
 return 0;
 }

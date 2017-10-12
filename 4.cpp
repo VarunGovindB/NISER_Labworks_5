03c7d1acdecf102722a7d8e4ef8aee892b75a468
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-10-12 23:39:45 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
+int lar(int n[],int x)
+{
+	int max;
+	max=n[0];
+	for(int i=0;i<(x-1);i++)
+	{
+		if(max<n[i+1])
+		max=n[i+1];
+	}
+	return max;
+}
 
+int sma(int n[],int x)
+{
+	int min;
+	min=n[0];
+	for(int i=0;i<(x-1);i++)
+	{
+		if(min>n[i+1])
+		min=n[i+1];
+	}
+	return min;
+}
 float sort(int n[],int x)
 {
-float t=0.0;
+float t;
 	for(int i=0;i<x;i++)
 	{
 		for(int j=0;j<i;j++)
@@ -15,41 +37,48 @@ float t=0.0;
 				n[j+1]=t;
 			}
 		}
-		
 	}
 	
-	for(int k=0;k<x;k++)
+            for(int i=0;i<x;i++)
 	{
-		cout<<n[k]<<" ";
+		cout<<n[i]<<" ";
 	}
-
-
-	
 }
 
 int main() 
 {
-	int x,y;
-	cout<<"Enter sizes of arrays:"<<endl;
-	cin>>x>>y;
-	int n1[x],n2[y];
-	cout<<"Enter "<<x<<" elements of array 1:";
+            int x,y;
+	cout<<"Enter size of array 1:";
+	cin>>x;
+	int n1[x];
+	cout<<"Enter "<<x<<" numbers:"<<endl;
 	for(int i=0;i<x;i++)
 	{
 		cin>>n1[i];
-		cout<<" ";
 	}
-	cout<<"Enter "<<y<<" elements of array 2:";
-	for(int j=0;j<y;j++)
+	cout<<"Enter size of array 2:";
+	cin>>y;
+	int n2[y];
+	cout<<"Enter "<<y<<" numbers:"<<endl;
+	for(int i=0;i<y;i++)
 	{
-		cin>>n2[j];
-		cout<<" ";
+		cin>>n2[i];
 	}
+	int c;
+	c=x+y;
+	int n[c];
+	for(int i=0;i<x;i++)
+	{
+		n[i]=n1[i];
+	}
+
+	for(int i=x,k=0;i<c,k<y;i++,k++)
+	{
+		n[i]=n2[k];
+	}
+	sort(n,c);
 	cout<<endl;
-	cout<<"Sorted array 1: ";
-	sort(n1,x);
-	cout<<endl;
-	cout<<"Sorted array 2: ";
-	sort(n2,y);
+            cout<<"Largest="<<lar(n,c)<<endl;
+	cout<<"Smallest="<<sma(n,c)<<endl;
 	return 0;
 }
